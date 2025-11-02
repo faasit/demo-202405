b54279e9a310b6b33fb646a2f094b1c814292f1e
Xdydy <dydy@i2ec.top>
2025-11-02 18:17:03 +0800

    feat: wordcount

--- wordcount/data/generate_data.py
+++ wordcount/data/generate_data.py
@@ -1,6 +1,7 @@
 import random
 import string
 import os
+import tqdm
 # 定义生成随机单词的函数
 def generate_random_word(min_length=3, max_length=10):
     length = random.randint(min_length, max_length)
@@ -10,14 +11,10 @@ def generate_random_word(min_length=3, max_length=10):
 target_size = 10 * 1024 * 1024  # 100MB
 
 # 初始化一个空字符串用于存储生成的单词
-current_size = 0
 
-with open('random_words.txt', 'w', encoding='utf-8') as file:
-    while current_size < target_size:
-        word = generate_random_word()
-        # 如果不是第一个单词，添加一个空格
-        current_size = os.path.getsize('random_words.txt')
-        file.write(word)
-        file.write(' ')
-
-print("已生成 10MB 的随机单词并保存到 random_words.txt 文件中。")
+for i in range(4):
+    current_size = 0
+    with open(f'stage0-{i}-input', 'w', encoding='utf-8') as file:
+        for _ in tqdm.tqdm(range(50000), desc=f"Generating stage0-{i}-input"):
+            word = generate_random_word()
+            file.write(word + ' ')
